65adc4a6e9659a12b4de4d094d9cfaf61fc70ab0
Aaron Davis <adbe455@gmail.com>
2019-10-02 13:16:00 -0400

    ad

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+// import { Hero } from './hero';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +12,18 @@ import { Injectable } from '@angular/core';
 export class SwapiService {
 
   constructor() { }
+
+  // searchSwapi(term: string): Observable<Hero[]> {
+  //   if (!term.trim()) {
+  //     // if not search term, return empty hero array.
+  //     return of([]);
+  //   }
+  //   return this.http.get<Swapi[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+  //     tap(_ => this.log(`found heroes matching "${term}"`)),
+  //     catchError(this.handleError<Swapi[]>('searchHeroes', []))
+  //   );
+  // }
+
+  // arr = ['luke', 'yoda', 'vader'];
+  // console.log(arr);
 }
