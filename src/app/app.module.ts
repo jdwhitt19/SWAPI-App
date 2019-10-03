d217b0b110d89699c3c333679f9671839970be2e
marthinusjt <marthinusjt@gmail.com>
2019-10-03 10:30:21 -0400

    mjt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
+// @angular
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
+// Components
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SearchComponent } from './search/search.component';
 import { DetailsComponent } from './details/details.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
+// Modules
+import { AppRoutingModule } from './app-routing.module';
 import { AppMaterialModule } from './app.material.module';
 
+// Services
 import { SwapiService } from './swapi.service';
 
 @NgModule({
